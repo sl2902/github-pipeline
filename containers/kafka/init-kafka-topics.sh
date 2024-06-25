20d10def5b68a79b89744b54fcc0c34b4acddaed
sl2902 <laxmsun@hotmail.com>
2024-06-25 20:16:08 +0530

    Add new topics

--- containers/kafka/init-kafka-topics.sh
+++ containers/kafka/init-kafka-topics.sh
@@ -6,6 +6,8 @@ kafka-topics --bootstrap-server kafka:29092 --list
 echo -e 'Creating kafka topics'
 kafka-topics --bootstrap-server kafka:29092 --create --if-not-exists --topic commits --replication-factor 1 --partitions 3
 kafka-topics --bootstrap-server kafka:29092 --create --if-not-exists --topic issues --replication-factor 1 --partitions 3
+kafka-topics --bootstrap-server kafka:29092 --create --if-not-exists --topic base_repo --replication-factor 1 --partitions 3
+kafka-topics --bootstrap-server kafka:29092 --create --if-not-exists --topic languages --replication-factor 1 --partitions 3
 
 echo -e 'Successfully created the following topics:'
 kafka-topics --bootstrap-server kafka:29092 --list
