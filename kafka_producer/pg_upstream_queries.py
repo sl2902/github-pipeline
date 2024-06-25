7c48cbfb20945aadab0ca233eb1d4efedd5ae6b9
sl2902 <laxmsun@hotmail.com>
2024-06-25 20:33:54 +0530

    Add query for new pipeline

--- kafka_producer/pg_upstream_queries.py
+++ kafka_producer/pg_upstream_queries.py
@@ -22,7 +22,7 @@ raw_qry_str = {
                     ,response->'commit'->'verification'->>'reason' as commit_verification_reason
                     ,(response->'commit'->'verification'->>'verified')::boolean as commit_verification_verified
                     ,response::text
-                    ,created_date
+                    ,created_date as load_date
                 
                 from 
                     gh_staging_raw_endpoints
@@ -56,11 +56,34 @@ raw_qry_str = {
                     ,(response ->'reactions'->>'rocket')::int AS rocket
                     ,(response ->'reactions'->>'eyes')::int AS eyes
                     ,response::text
-                    ,created_date
+                    ,created_date as load_date
                 FROM 
                     gh_staging_raw_endpoints
                 where 
                     endpoint = 'issues'
 
-                """
+                """,
+    "/":        """
+                select 
+                    response ->'id' as stat_id
+                    ,owner
+                    ,response ->>'name' as repo
+                    ,(response ->>'forks')::int as fork_count
+                    ,(response ->>'size')::int as size_count
+                    ,(response ->>'watchers')::int as watchers_count
+                    ,(response ->>'open_issues')::int as open_issues_count
+                    ,(response ->>'network_count')::int as network_count
+                    ,(response ->>'stargazers_count')::int as stargazers_count
+                    ,(response ->>'subscribers_count')::int as subscribers_count
+                    ,(response ->>'created_at')::timestamp as created_at
+                    ,(response ->>'pushed_at')::timestamp as pushed_at
+                    ,(response ->>'updated_at')::timestamp as updated_at
+                    ,response::text
+                    ,created_date as load_date
+                from
+                    gh_staging_raw_endpoints
+                where 
+                    endpoint = '/'
+
+                """ 
 }
