c77572692051dbf59c84c3086090ba0154894457
sl2902 <laxmsun@hotmail.com>
2024-06-30 01:49:25 +0530

    Updated partition keys

--- utils/iceberg_kafka_keys.py
+++ utils/iceberg_kafka_keys.py
@@ -1,14 +1,14 @@
 topic_keys = {
     "commits": {
         "primary_key": "commit_sha",
-        "partition_keys": ["owner", "days(commit_author_date)"]
+        "partition_keys": ["repo", "commit_author_date"]
     },
     "issues": {
         "primary_key": "id",
-        "partition_keys": ["owner", "days(created_at)"]
+        "partition_keys": ["repo", "created_at"]
     },
     "base_repo": {
         "primary_key": "stat_id",
-        "partition_keys": ["owner", "days(updated_at)"]
+        "partition_keys": ["repo", "updated_at"]
     }
 }
