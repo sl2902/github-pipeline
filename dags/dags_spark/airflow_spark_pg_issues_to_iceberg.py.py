c4a5a18da6b3cdae24d037c44e53001ddf0b7975
sl2902 <laxmsun@hotmail.com>
2024-07-01 02:02:23 +0530

    Updated spark submit to include start date and end date

--- dags/dags_spark/airflow_spark_pg_issues_to_iceberg.py.py
+++ dags/dags_spark/airflow_spark_pg_issues_to_iceberg.py.py
@@ -52,29 +52,73 @@ dag = DAG(
 )
 
 
-run_date = "{{ dag_run.conf['execution_date'] if dag_run and dag_run.conf and 'execution_date' in dag_run.conf else ds_nodash }}"
+# run_date = "{{ dag_run.conf['execution_date'] if dag_run and dag_run.conf and 'execution_date' in dag_run.conf else ds_nodash }}"
 
+def extract_dates(**kwargs):
+    dag_run = kwargs.get('dag_run')
+    start_date = dag_run.conf.get('start_date') if dag_run and 'start_date' in dag_run.conf else ''
+    end_date = dag_run.conf.get('end_date') if dag_run and 'end_date' in dag_run.conf else ''
+    
+    # Log dates
+    logger.info(f'Start date {start_date}')
+    logger.info(f'End date {end_date}')
+
+    # Set default dates if they are not provided
+    if not start_date:
+        start_date = datetime.now().replace(minute=0, second=0, microsecond=0)
+    else:
+        start_date = datetime.strptime(start_date, '%Y-%m-%d %H:%M:%S')
+
+    if not end_date:
+        end_date = start_date + timedelta(hours=1)
+
+    # Format dates as strings
+    start_date_str = start_date.strftime('%Y-%m-%d %H:%M:%S')
+    end_date_str = end_date.strftime('%Y-%m-%d %H:%M:%S')
+
+    # Return dates to be used by downstream tasks
+    return {'start_date': f'{start_date_str}', 'end_date': f'{end_date_str}'}
+
+extract_dates_task = PythonOperator(
+    task_id='extract_dates',
+    provide_context=True,
+    python_callable=extract_dates,
+    dag=dag
+)
 
 
 SSHHook = SSHHook(ssh_conn_id="ssh-conn", key_file="/home/airflow/.ssh/id_ecdsa", cmd_timeout=None)
 
-
-spark_issues_transfer = SSHOperator(
-    task_id="spark_issues_transfer",
-    command=f"""
+templated_command = """
     export PYTHONPATH=/opt/spark:/opt/spark/kafka_consumer/:/opt/spark/utils/:$PYTHONPATH && 
-    export BOOTSTRAP_SERVER={os.getenv('BOOTSTRAP_SERVER')} &&
-    export POSTGRES_DB={os.getenv('POSTGRES_DB')} &&
-    export POSTGRES_USER={os.getenv('POSTGRES_USER')} &&
-    export POSTGRES_PASSWORD={os.getenv('POSTGRES_PASSWORD')} &&
-    export POSTGRES_PORT={os.getenv('PORT')} &&
-    export POSTGRES_HOST={os.getenv('POSTGRES_HOST')} &&
-    export S3_LOCATION={os.getenv('S3_LOCATION')} &&
+    export BOOTSTRAP_SERVER={0} &&
+    export POSTGRES_DB={1} &&
+    export POSTGRES_USER={2} &&
+    export POSTGRES_PASSWORD={3} &&
+    export POSTGRES_PORT={4} &&
+    export POSTGRES_HOST={5} &&
+    export S3_LOCATION={6} &&
     export AWS_ACCESS_KEY_ID=minio &&
     export AWS_SECRET_ACCESS_KEY=minio123 &&
     export AWS_REGION=us-east-1 &&
-    /opt/spark/bin/spark-submit --master spark://spark-master:7077 --deploy-mode client /opt/spark/spark_batch/pg_to_iceberg.py --table issues
-    """,
+    /opt/spark/bin/spark-submit --master spark://spark-master:7077 --deploy-mode client \
+        /opt/spark/spark_batch/pg_to_iceberg.py \
+            --table issues \
+            --start_date '{{{{ ti.xcom_pull(task_ids='extract_dates')['start_date'] }}}}' \
+            --end_date '{{{{ ti.xcom_pull(task_ids='extract_dates')['end_date'] }}}}'
+""".format(
+    os.getenv('BOOTSTRAP_SERVER'),
+    os.getenv('POSTGRES_DB'),
+    os.getenv('POSTGRES_USER'),
+    os.getenv('POSTGRES_PASSWORD'),
+    os.getenv('PORT'),
+    os.getenv('POSTGRES_HOST'),
+    os.getenv('S3_LOCATION')
+)
+
+spark_issues_transfer = SSHOperator(
+    task_id="spark_issues_transfer",
+    command=templated_command,
     ssh_hook=SSHHook,
     dag=dag
 )
@@ -85,4 +129,4 @@ trigger_dbt_issues_model = TriggerDagRunOperator(
     dag=dag
 )
 
-spark_issues_transfer >> trigger_dbt_issues_model
+extract_dates_task >> spark_issues_transfer >> trigger_dbt_issues_model
