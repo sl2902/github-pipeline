af7789785adbee01ecd634ed3d314f1661dc4487
sl2902 <laxmsun@hotmail.com>
2024-07-02 02:54:02 +0530

    Add upstream ddl for pypi

--- ddl/gh_ddl.sql
+++ ddl/gh_ddl.sql
@@ -30,3 +30,28 @@ CREATE TABLE gh_track_raw_endpoints (
         status VARCHAR(10),
         last_updated_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
+
+DROP TABLE IF EXISTS pypi_staging_raw_endpoints;
+
+CREATE TABLE pypi_staging_raw_endpoints (
+    id SERIAL PRIMARY KEY,
+    package VARCHAR(50),
+    endpoint VARCHAR(30),
+    response JSONB,
+    response_md5 CHAR(32),
+    load_date TIMESTAMP NOT NULL DEFAULT NOW(),
+    UNIQUE (endpoint, response_md5)
+);
+
+DROP TABLE IF EXISTS pypi_track_raw_endpoints;
+
+CREATE TABLE pypi_track_raw_endpoints (
+        id SERIAL PRIMARY KEY,
+        package VARCHAR(50),
+        endpoint VARCHAR(20),
+        start_date DATE,
+        end_date DATE,
+        load_type VARCHAR(10),
+        status VARCHAR(10),
+        last_updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
